c5fde3a779215e99b9031506534e160b137236bd
ajay.kushwaha <ajay.kushwaha@rakute.com>
2021-08-26 02:29:28 +0530

    dsa project structure

---
+++ src/main/java/com/dsa/DsaFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaFundamentalsApplication.class, args);
+	}
+
+}
